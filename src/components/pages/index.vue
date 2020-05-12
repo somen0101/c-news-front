bb361b64c55b4c3be8d9037efbb6f2a86cbe7a4d
somen0101 <so_men0101@outlook.jp>
2020-05-12 23:40:26 +0900

    コミット

---
+++ src/components/pages/index.vue
@@ -0,0 +1,193 @@
+<template>
+   <v-row justify="center">
+       <v-col cols="12"
+        align="center">
+        <h2 class="mb-1 news-category">{{ page_title }}</h2> 
+        <v-divider class="mb-3" />
+        <v-card
+          v-for="news in info"
+          :key="news.title"
+          outlined
+          hover
+          class="mb-3"
+        >
+         <v-container class="pt-3 pb-0">
+         <a :href = "news.topic_url" target="_brank" rel="noopener">
+             <v-row>
+             <v-col class="text-left py-1 pb-0" cols="8">
+                  <h3 class="mb-2" icon><icon v-if="checktoken()">
+                  <v-icon v-if="emotion_num(news.sentimental) > 0.4">mdi-emoticon-excited-outline</v-icon>
+                  <v-icon v-else-if="emotion_num(news.sentimental) > -0.3">mdi-emoticon-neutral-outline</v-icon>
+                  <v-icon v-else>mdi-emoticon-sad</v-icon>
+                  </icon>{{ news.title }}</h3>
+                  <p class="mb-0 text-right">
+                    <span class="mr-2">{{ news.author }}</span>
+                    <time>{{ getFormtedDate(news.published_date) }}</time>
+                   </p>
+                   <v-card-text class="news-text pl-1 py-2">
+                      {{ getNewsText(news.description) }}
+                    </v-card-text> 
+             </v-col>
+             <v-col class="px-5" cols="4">
+                  <v-img
+                    :src="getImageUrl(news.image_url)"
+                    :height="$vuetify.breakpoint.smAndUp ? 150 : 90"
+                    style="border-radius: 6px;"
+                  />
+                </v-col>
+            </v-row> 
+         </a>
+         </v-container>
+        </v-card>
+       </v-col>
+   </v-row>
+</template>
+
+<script>
+import axios from 'axios'
+import dayjs from'dayjs'
+export default {
+  name: 'index',
+  data () {
+    return {
+      domain_tags: [],
+      page_path: [],
+      page_title: '国内トップニュース',  
+      info: [],
+      credit:[],
+    }
+  },
+  watch: {
+    '$route' (to) {   
+     this.domain_tags = to.params.domain_tags;
+     this.page_path = to.path
+     if (this.page_path == '/') {  
+    axios.get('http://localhost:8000/api/newstopics/?domain_tags=jp&top_news=top').then(
+        response => this.info = response.data) 
+     this.page_title = '国内トップニュース'    
+        } else if(this.page_path == '/news/jp') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=jp').then(
+        response => this.info = response.data)  
+     this.page_title = '国内記事'
+        } else if(this.page_path == '/news/us') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=us').then(
+        response => this.info = response.data)
+        this.page_title = 'アメリカ'  
+    } else if(this.page_path == '/news/gb') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=gb').then(
+        response => this.info = response.data)
+        this.page_title = 'イギリス'      
+    } else if(this.page_path == '/news/kr') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=kr').then(
+        response => this.info = response.data)
+        this.page_title = '韓国'
+    } else if(this.page_path == '/news/fr') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=fr').then(
+        response => this.info = response.data)
+        this.page_title = 'フランス'
+     }else if(this.page_path == '/news/it') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=it').then(
+        response => this.info = response.data)
+        this.page_title = 'イタリア'
+     }else if(this.page_path == '/news/de') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=de').then(
+        response => this.info = response.data)
+        this.page_title = 'ドイツ'}
+    }
+  },
+  mounted () {
+    this.domain_tags = this.$route.params.domain_tags;
+    this.page_path = this.$route.path
+    if (this.page_path == '/') {  
+    axios.get('http://localhost:8000/api/newstopics/?domain_tags=jp&top_news=top').then(
+        response => this.info = response.data)  
+        } else if(this.page_path == '/news/jp') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=jp').then(
+        response => this.info = response.data)  
+
+        } else if(this.page_path == '/news/us') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=us').then(
+        response => this.info = response.data)  
+    } else if(this.page_path == '/news/gb') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=gb').then(
+        response => this.info = response.data)  
+    
+    } else if(this.page_path == '/news/kr') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=kr').then(
+        response => this.info = response.data)
+    } else if(this.page_path == '/news/fr') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=fr').then(
+        response => this.info = response.data)
+     }else if(this.page_path == '/news/it') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=it').then(
+        response => this.info = response.data)
+     }else if(this.page_path == '/news/de') {
+            axios.get('http://localhost:8000/api/newstopics/?domain_tags=de').then(
+        response => this.info = response.data)}},
+   methods: {
+       checktoken() {
+        if (this.$session.has("token")) {
+          return true
+        }else{
+          return false
+        }
+      },
+   getFormtedDate(date) {
+      return dayjs(date).format("M/DD HH:mm");
+    },
+   getImageUrl(imageUrl) {
+      if (imageUrl !== null && imageUrl.match(/^https?:\/\//)) {
+        return imageUrl;
+      } else {
+        return require("@/assets/img/noimage.jpg");
+      }
+    },
+    getNewsText(text) {
+      const limitedText = text.substr(0, 120);
+      if (limitedText.slice(-3) === "...") {
+        return limitedText;
+      }
+      return limitedText + "...";
+    },
+    emotion_num(num) {
+      return Number(num);
+    },  
+
+
+   }   
+}
+</script>
+
+<style lang="scss" scoped>
+.news-category {
+  color: #616161;
+  @media screen and (max-width: 600px) {
+    font-size: 20px;
+  }
+}
+.news-title {
+  font-size: 17px;
+  color: #616161;
+  @media screen and (max-width: 600px) {
+    font-size: 13px;
+  }
+}
+.news-author {
+  font-size: 11.5px;
+  color: #757575;
+}
+.news-text {
+  font-size: 12px;
+  color: #757575;
+}
+a {
+  text-decoration: none;
+}
+.share-btn {
+  opacity: 0.4;
+  &:hover {
+    transition: all 0.2s ease;
+    opacity: 1;
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Auth from '@/components/pages/Auth'
+import createuser from '@/components/pages/Createuser'
+import Index from '@/components/pages/index'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index,
+  },
+
+  {
+    path: '/news/:domain_tags',
+    name: 'news',
+    component: Index,
+  },
+
+  {
+    path: '/auth',
+    name: 'Auth',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Auth
+  },
+  {
+    path: '/createuser',
+    name: 'Createuser',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: createuser
+  },
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
