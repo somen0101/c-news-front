ca64023fe31f607daa4554b9f09fbc00a8e36014
minddrop <ryooooooo1@gmail.com>
2020-05-17 15:37:03 +0900

    fixed bookmark-button

---
+++ src/components/pages/Bookmark.vue
@@ -0,0 +1,60 @@
+
+<template>
+  <div class="text-right" v-if="checktoken()">
+    <v-btn small class="bm-btn" @click="bookmark">
+      <v-icon>mdi-link</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: [
+    "title",
+    "sentimental",
+    "author",
+    "pubilshed_date",
+    "description",
+    "image_url",
+    "topic_url"
+  ],
+  data() {
+    return {
+      isLike: false
+    };
+  },
+  methods: {
+    bookmark() {
+      const body = {
+        username: this.$session.get("username"),
+        title: this.title,
+        description: this.sentimental,
+        published_date: this.published_date,
+        author: this.author,
+        topic_url: this.topic_url,
+        image_url: this.image_url,
+        sentimental: this.sentimental
+      };
+      axios.post("http://localhost:8000/api/bookmark/", body);
+    },
+    checktoken() {
+      if (this.$session.has("token")) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.bm-btn {
+  opacity: 0.4;
+  &:hover {
+    transition: all 0.2s ease;
+    opacity: 1;
+  }
+}
+</style>
--- src/components/pages/index.vue
+++ src/components/pages/index.vue
@@ -1,193 +1,206 @@
 <template>
-   <v-row justify="center">
-       <v-col cols="12"
-        align="center">
-        <h2 class="mb-1 news-category">{{ page_title }}</h2> 
-        <v-divider class="mb-3" />
-        <v-card
-          v-for="news in info"
-          :key="news.title"
-          outlined
-          hover
-          class="mb-3"
-        >
-         <v-container class="pt-3 pb-0">
-         <a :href = "news.topic_url" target="_brank" rel="noopener">
-             <v-row>
-             <v-col class="text-left py-1 pb-0" cols="8">
-                  <h3 class="mb-2" icon>
-                    <!--  
-                      <icon>
+  <v-row justify="center">
+    <v-col cols="12" align="center">
+      <h2 class="mb-1 news-category">{{ page_title }}</h2>
+      <v-divider class="mb-3" />
+      <v-card v-for="news in info" :key="news.title" outlined hover class="mb-3">
+        <v-container class="pt-3 pb-0">
+          <a :href="news.topic_url" target="_brank" rel="noopener">
+            <v-row>
+              <v-col class="text-left py-1 pb-0" cols="8">
+                <h3 class="mb-2" icon>
                   <v-icon v-if="emotion_num(news.sentimental) > 0.4">mdi-emoticon-excited-outline</v-icon>
-                  <v-icon v-else-if="emotion_num(news.sentimental) > -0.3">mdi-emoticon-neutral-outline</v-icon>
+                  <v-icon
+                    v-else-if="emotion_num(news.sentimental) > -0.3"
+                  >mdi-emoticon-neutral-outline</v-icon>
                   <v-icon v-else>mdi-emoticon-sad</v-icon>
-                  </icon>
-                  -->
-                  {{ news.title }}</h3>
-                  <p class="mb-0 text-right">
-                    <span class="mr-2">{{ news.author }}</span>
-                    <time>{{ getFormtedDate(news.published_date) }}</time>
-                   </p>
-                   <v-card-text class="news-text pl-1 py-2">
-                      {{ getNewsText(news.description) }}
-                    </v-card-text> 
-             </v-col>
-             <v-col class="px-5" cols="4">
-                  <v-img
-                    :src="getImageUrl(news.image_url)"
-                    :height="$vuetify.breakpoint.smAndUp ? 150 : 90"
-                    style="border-radius: 6px;"
-                  />
-                </v-col>
-            </v-row> 
-         </a>
-         <!--
-          <div class="text-right" v-if="checktoken()">
-          <v-btn small class="share-btn"  
-              title="news.title"
-              sentimental="news.sentimental"
-              author="news.author"
-              published_date="news.published_date"
-              description="news.description"
-              image_url="news.image_url"
-              topic_url="news.topic_url"
-               @click="bookmark">
-         <v-icon>mdi-link
-        </v-icon>
-        </v-btn>
-        </div>
-        -->
-         </v-container>
-        </v-card>
-       </v-col>
-   </v-row>
+                  {{ news.title }}
+                </h3>
+                <p class="mb-0 text-right">
+                  <span class="mr-2">{{ news.author }}</span>
+                  <time>{{ getFormtedDate(news.published_date) }}</time>
+                </p>
+                <v-card-text class="news-text pl-1 py-2">{{ getNewsText(news.description) }}</v-card-text>
+              </v-col>
+              <v-col class="px-5" cols="4">
+                <v-img
+                  :src="getImageUrl(news.image_url)"
+                  :height="$vuetify.breakpoint.smAndUp ? 150 : 90"
+                  style="border-radius: 6px;"
+                />
+              </v-col>
+            </v-row>
+          </a>
+          <Bookmark
+            :title="news.title"
+            :sentimental="news.sentimental"
+            :author="news.author"
+            :published_date="news.published_date"
+            :description="news.description"
+            :image_url="news.image_url"
+            :topic_url="news.topic_url"
+          ></Bookmark>
+        </v-container>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import axios from 'axios'
-import dayjs from'dayjs'
+import axios from "axios";
+import dayjs from "dayjs";
+import Bookmark from "./Bookmark.vue";
+
 export default {
-  name: 'index',
-  data () {
+  name: "index",
+  components: {
+    Bookmark
+  },
+  data() {
     return {
       domain_tags: [],
       page_path: [],
-      page_title: '国内トップニュース',  
+      page_title: "国内トップニュース",
       info: [],
-      credit:[],
-      username:null,
-    }
+      credit: [],
+      username: null
+    };
   },
   watch: {
-    '$route' (to) {   
-     this.domain_tags = to.params.domain_tags;
-     this.page_path = to.path
-     if (this.page_path == '/') {  
-    axios.get('http://localhost:8000/api/newstopics/?domain_tags=jp&top_news=top').then(
-        response => this.info = response.data) 
-     this.page_title = '国内トップニュース'    
-        } else if(this.page_path == '/news/jp') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=jp').then(
-        response => this.info = response.data)  
-     this.page_title = '国内記事'
-        } else if(this.page_path == '/news/us') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=us').then(
-        response => this.info = response.data)
-        this.page_title = 'アメリカ'  
-    } else if(this.page_path == '/news/gb') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=gb').then(
-        response => this.info = response.data)
-        this.page_title = 'イギリス'      
-    } else if(this.page_path == '/news/kr') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=kr').then(
-        response => this.info = response.data)
-        this.page_title = '韓国'
-    } else if(this.page_path == '/news/fr') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=fr').then(
-        response => this.info = response.data)
-        this.page_title = 'フランス'
-     }else if(this.page_path == '/news/it') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=it').then(
-        response => this.info = response.data)
-        this.page_title = 'イタリア'
-     }else if(this.page_path == '/news/de') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=de').then(
-        response => this.info = response.data)
-        this.page_title = 'ドイツ'
-       /* 
-        }else if(this.page_path == '/bookmark/'+this.$session.get("username")) {
-            axios.get('http://localhost:8000/api/bookmark/?username='+this.$session.get("username")).then(
-        response => this.info = response.data)
-         this.page_title = 'あなたのブックマーク'
-         */
-        }
+    $route(to) {
+      this.domain_tags = to.params.domain_tags;
+      this.page_path = to.path;
+      if (this.page_path == "/") {
+        axios
+          .get(
+            "http://localhost:8000/api/newstopics/?domain_tags=jp&top_news=top"
+          )
+          .then(response => (this.info = response.data));
+        this.page_title = "国内トップニュース";
+      } else if (this.page_path == "/news/jp") {
+        axios
+          .get("http://localhost:8000/api/newstopics/?domain_tags=jp")
+          .then(response => (this.info = response.data));
+        this.page_title = "国内記事";
+      } else if (this.page_path == "/news/us") {
+        axios
+          .get("http://localhost:8000/api/newstopics/?domain_tags=us")
+          .then(response => (this.info = response.data));
+        this.page_title = "アメリカ";
+      } else if (this.page_path == "/news/gb") {
+        axios
+          .get("http://localhost:8000/api/newstopics/?domain_tags=gb")
+          .then(response => (this.info = response.data));
+        this.page_title = "イギリス";
+      } else if (this.page_path == "/news/kr") {
+        axios
+          .get("http://localhost:8000/api/newstopics/?domain_tags=kr")
+          .then(response => (this.info = response.data));
+        this.page_title = "韓国";
+      } else if (this.page_path == "/news/fr") {
+        axios
+          .get("http://localhost:8000/api/newstopics/?domain_tags=fr")
+          .then(response => (this.info = response.data));
+        this.page_title = "フランス";
+      } else if (this.page_path == "/news/it") {
+        axios
+          .get("http://localhost:8000/api/newstopics/?domain_tags=it")
+          .then(response => (this.info = response.data));
+        this.page_title = "イタリア";
+      } else if (this.page_path == "/news/de") {
+        axios
+          .get("http://localhost:8000/api/newstopics/?domain_tags=de")
+          .then(response => (this.info = response.data));
+        this.page_title = "ドイツ";
+      } else if (
+        this.page_path ==
+        "/bookmark/" + this.$session.get("username")
+      ) {
+        axios
+          .get(
+            "http://localhost:8000/api/bookmark/?username=" +
+              this.$session.get("username")
+          )
+          .then(response => (this.info = response.data));
+        this.page_title = "あなたのブックマーク";
+      }
     }
   },
-  mounted () {
+  mounted() {
     this.domain_tags = this.$route.params.domain_tags;
-    this.page_path = this.$route.path
-    if (this.page_path == '/') {  
-    axios.get('http://localhost:8000/api/newstopics/?domain_tags=jp&top_news=top').then(
-        response => this.info = response.data)  
-        } else if(this.page_path == '/news/jp') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=jp').then(
-        response => this.info = response.data)  
-
-        } else if(this.page_path == '/news/us') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=us').then(
-        response => this.info = response.data)  
-    } else if(this.page_path == '/news/gb') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=gb').then(
-        response => this.info = response.data)  
-    
-    } else if(this.page_path == '/news/kr') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=kr').then(
-        response => this.info = response.data)
-    } else if(this.page_path == '/news/fr') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=fr').then(
-        response => this.info = response.data)
-     }else if(this.page_path == '/news/it') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=it').then(
-        response => this.info = response.data)
-     }else if(this.page_path == '/news/de') {
-            axios.get('http://localhost:8000/api/newstopics/?domain_tags=de').then(
-        response => this.info = response.data)
-      /*  
-        }else if(this.page_path == '/bookmark/'+this.$session.get("username")) {
-            axios.get('http://localhost:8000/api/bookmark/?username='+this.$session.get("username")).then(
-        response => this.info = response.data)
-        */
-       }},
-   methods: {  
-       /*
-       bookmark() {
-           this.username=this.$session.get("username")
-           axios.post('http://localhost:8000/api/bookmark/',
-            {
-             body: {
-                "username": this.username,
-                "title": String(event.target.title),
-                "description": String(event.target.sentimental),
-                "published_date": String(event.target.published_date),
-                "author": String(event.target.author),
-                "topic_url": String(event.target.topic_url),
-                "image_url": String(event.target.image_url),
-                "sentimental": String(event.target.sentimental)
-             },
-          })    
-          }, 
-        */   
-       checktoken() {
-        if (this.$session.has("token")) {
-          return true
-        }else{
-          return false
+    this.page_path = this.$route.path;
+    if (this.page_path == "/") {
+      axios
+        .get(
+          "http://localhost:8000/api/newstopics/?domain_tags=jp&top_news=top"
+        )
+        .then(response => (this.info = response.data));
+    } else if (this.page_path == "/news/jp") {
+      axios
+        .get("http://localhost:8000/api/newstopics/?domain_tags=jp")
+        .then(response => (this.info = response.data));
+    } else if (this.page_path == "/news/us") {
+      axios
+        .get("http://localhost:8000/api/newstopics/?domain_tags=us")
+        .then(response => (this.info = response.data));
+    } else if (this.page_path == "/news/gb") {
+      axios
+        .get("http://localhost:8000/api/newstopics/?domain_tags=gb")
+        .then(response => (this.info = response.data));
+    } else if (this.page_path == "/news/kr") {
+      axios
+        .get("http://localhost:8000/api/newstopics/?domain_tags=kr")
+        .then(response => (this.info = response.data));
+    } else if (this.page_path == "/news/fr") {
+      axios
+        .get("http://localhost:8000/api/newstopics/?domain_tags=fr")
+        .then(response => (this.info = response.data));
+    } else if (this.page_path == "/news/it") {
+      axios
+        .get("http://localhost:8000/api/newstopics/?domain_tags=it")
+        .then(response => (this.info = response.data));
+    } else if (this.page_path == "/news/de") {
+      axios
+        .get("http://localhost:8000/api/newstopics/?domain_tags=de")
+        .then(response => (this.info = response.data));
+    } else if (this.page_path == "/bookmark/" + this.$session.get("username")) {
+      axios
+        .get(
+          "http://localhost:8000/api/bookmark/?username=" +
+            this.$session.get("username")
+        )
+        .then(response => (this.info = response.data));
+    }
+  },
+  methods: {
+    bookmark(event) {
+      alert(event.target);
+      console.log(event.target);
+      const body = {
+        body: {
+          username: this.$session.get("username"),
+          title: String(event.target.title),
+          description: String(event.target.sentimental),
+          published_date: String(event.target.published_date),
+          author: String(event.target.author),
+          topic_url: String(event.target.topic_url),
+          image_url: String(event.target.image_url),
+          sentimental: String(event.target.sentimental)
         }
-      },
-   getFormtedDate(date) {
+      };
+      axios.post("http://localhost:8000/api/bookmark/", body);
+    },
+    checktoken() {
+      if (this.$session.has("token")) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    getFormtedDate(date) {
       return dayjs(date).format("M/DD HH:mm");
     },
-   getImageUrl(imageUrl) {
+    getImageUrl(imageUrl) {
       if (imageUrl !== null && imageUrl.match(/^https?:\/\//)) {
         return imageUrl;
       } else {
@@ -203,11 +216,9 @@ export default {
     },
     emotion_num(num) {
       return Number(num);
-    },  
-
-
-   }   
-}
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -234,12 +245,5 @@ export default {
 }
 a {
   text-decoration: none;
-}
-.share-btn {
-  opacity: 0.4;
-  &:hover {
-    transition: all 0.2s ease;
-    opacity: 1;
-  }
 }
 </style>
